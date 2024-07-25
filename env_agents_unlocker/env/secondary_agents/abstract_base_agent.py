1d970becb3507c60ce411cecc7fb6bc74dee7410
JulienT01 <julien.tj@gmail.com>
2024-07-25 12:02:57 +0200

    add an "id" and "to_dict()" function

--- env_agents_unlocker/env/secondary_agents/abstract_base_agent.py
+++ env_agents_unlocker/env/secondary_agents/abstract_base_agent.py
@@ -3,21 +3,16 @@ from abc import ABC, abstractmethod
 from env_agents_unlocker.env.actions.basic_action import AbstractBaseAction
 
 
-
-
 ####### TODO? #######
 
-    # - une liste d'actions potentielles
-    # - une liste d'action débloquées
-    # - une fonction objectif qu'il cherche a maximiser selon les actions disponible qu'il a: 
-    #     - le nombre d'action disponnible ?
-    #     - chaque action à une durée et une valeur, et le but est de maximiser les valeurs sur un certains temps donné
-
-
+# - une liste d'actions potentielles
+# - une liste d'action débloquées
+# - une fonction objectif qu'il cherche a maximiser selon les actions disponible qu'il a:
+#     - le nombre d'action disponnible ?
+#     - chaque action à une durée et une valeur, et le but est de maximiser les valeurs sur un certains temps donné
 
 
 #####################
-
 
 
 class AbstractBaseAgent(ABC):
@@ -25,21 +20,27 @@ class AbstractBaseAgent(ABC):
     Base class for all Agents living in the environment.
     The other agent should inherite from this class
     """
-    
-    def __init__(self, potential_actions:list[AbstractBaseAction], pre_unlock_actions:list[str] = None) -> None:
+
+    def __init__(
+        self,
+        name: str,
+        potential_actions: list[AbstractBaseAction],
+        pre_unlock_actions: list[str] = None,
+    ) -> None:
         """
         ------------ Parameters ------------
+        name (str):
+            The name of the agent (could be use as ID)
         potential_actions (list[AbstractBaseAction]):
             List of all the actions for this agent (lock and unlock)
-        pre_unlock_actions (str ou list[str,]) , default value = None : 
+        pre_unlock_actions (str ou list[str,]) , default value = None :
             actions to unlock from the begining (by default all actions are locked)
         """
+        self.name = name
         self.potential_actions = potential_actions
 
         if pre_unlock_actions is not None:
             self.unlock_actions(pre_unlock_actions)
-
-
 
     def unlock_actions(self, actions_to_unlock):
         """
@@ -47,16 +48,16 @@ class AbstractBaseAgent(ABC):
 
         Warning : there is no feedback about the previous state of the action, or even if the action exist for this agent.
         The function can be overwite if you need a specific functionality.
-        
+
         ------------ Parameters ------------
-        actions_to_unlock (str ou list[str,]) : 
+        actions_to_unlock (str ou list[str,]) :
             actions to unlock
-        
+
         ------------ Returns ---------------
         None
 
         """
-        if isinstance(actions_to_unlock,str):
+        if isinstance(actions_to_unlock, str):
             actions_to_unlock = [actions_to_unlock]
 
         for action in self.potential_actions:
@@ -66,7 +67,6 @@ class AbstractBaseAgent(ABC):
     def get_all_actions_names(self):
         return [action.get_name() for action in self.potential_actions]
 
-
     @abstractmethod
     def get_current_score(self):
         """
@@ -74,5 +74,9 @@ class AbstractBaseAgent(ABC):
         """
         pass
 
-
-
+    @abstractmethod
+    def to_dict(self):
+        """
+        Return a dict that describe the current agent.
+        """
+        pass
--- env_agents_unlocker/env/secondary_agents/basic_agent.py
+++ env_agents_unlocker/env/secondary_agents/basic_agent.py
@@ -1,21 +1,27 @@
-from env_agents_unlocker.env.secondary_agents.abstract_base_agent import AbstractBaseAgent
+from env_agents_unlocker.env.secondary_agents.abstract_base_agent import (
+    AbstractBaseAgent,
+)
 from env_agents_unlocker.env.actions.basic_action import AbstractBaseAction
 
 
-
 class BasicAgent(AbstractBaseAgent):
-
-    def __init__(self, potential_actions:list[AbstractBaseAction], pre_unlock_actions:list[str] = None) -> None:
+    def __init__(
+        self,
+        name: str,
+        potential_actions: list[AbstractBaseAction],
+        pre_unlock_actions: list[str] = None,
+    ) -> None:
         """
         Basic Agent, its objective (to maximize its reward) is to unlock the maximum of its actions
         ------------ Parameters ------------
+        name (str):
+            The name of the agent (could be use as ID)
         potential_actions (list[AbstractBaseAction]):
             List of all the actions for this agent (lock and unlock)
-        pre_unlock_actions (str ou list[str,]) , default value = None : 
+        pre_unlock_actions (str ou list[str,]) , default value = None :
             actions to unlock from the begining (by default all actions are locked)
         """
-        super().__init__(potential_actions,pre_unlock_actions)
-
+        super().__init__(name, potential_actions, pre_unlock_actions)
 
     def get_current_reward(self):
         """
@@ -23,7 +29,7 @@ class BasicAgent(AbstractBaseAgent):
 
         ------------ Parameters ------------
         None
-        
+
         ------------ Returns ---------------
         return int :
             the current reward for this agent
@@ -36,7 +42,7 @@ class BasicAgent(AbstractBaseAgent):
 
         ------------ Parameters ------------
         None
-        
+
         ------------ Returns ---------------
         return list[AbstractBaseAction] :
             the list of all the unlocked actions
@@ -47,6 +53,15 @@ class BasicAgent(AbstractBaseAgent):
         for action in self.potential_actions:
             if action.is_unlock():
                 unlocked_action.append(action)
-        
+
         return unlocked_action
 
+    def to_dict(self):
+        dict_to_return = {
+            "name": self.name,
+            "all_actions": list(map(lambda x: x.to_dict(), self.potential_actions)),
+            "unlocked_action": list(
+                map(lambda x: x.to_dict(), self._get_unlocked_actions())
+            ),
+        }
+        return dict_to_return
